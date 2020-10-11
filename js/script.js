4831e44ca13d6e0a7ba42e001013ece9861964e3
Francisndungu2 <francianmbugua@gmail.com>
2020-10-11 21:18:23 +0300

    adddevelopment js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+            $(".show1").click(function() {
+                $("#hide1").show(3000);
+                $(".show1").hide(3000);
+            })
+            $("#hide1").click(function() {
+                $("#hide1").hide(3000);
+                $(".show1").show(3000);
+            })
